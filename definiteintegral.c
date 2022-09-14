c6f62a5cfb7c89e05241590abf347c5bfd9c3d64
Jesus Navarro <45080480+jesus1112@users.noreply.github.com>
2022-09-14 15:19:37 -0600

    Create definiteintegral.c
    Source code

---
+++ definiteintegral.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+#define DX    (1E-6)   /* Delta x = 1x10-6 */
+
+/* Function definition */
+#define FX(x) ((x)*(x)*(x))
+
+
+int main(void)
+{
+	double xa=0, xb=5;
+	double x, s;
+	
+	/* Left Riemmann sum */
+	for (x=xa,s=0; x<xb; x+=DX)
+		s += FX(x)*DX;
+	printf("%.15f\n", s);
+	
+	/* Midpoint sum */
+	for (x=xa+DX/2.0,s=0; x<xb; x+=DX)
+		s += FX(x)*DX;
+	printf("%.15f\n", s);
+	
+	/* Trapezoidal sum */
+	for (x=xa,s=0; x<xb; x+=DX)
+		s += (FX(x)+FX(x+DX))/2.0*DX;
+	printf("%.15f\n", s);
+	
+	return 0;
+}
